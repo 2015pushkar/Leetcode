4c6cbc45baf2270ad32829a318e020b502be4672
Pushkar <61586999+2015pushkar@users.noreply.github.com>
2025-12-24 10:29:22 -0500

    add bit masking to isUnique

--- Arrays_&_Hashing/isUnique.py
+++ Arrays_&_Hashing/isUnique.py
@@ -9,7 +9,21 @@ Implementation:
 1. If str.length>128: return False (cannot form a string of unique characters)
 2. Use boolean arr[128] and set the flag if the character i is contained in the string
 """
-def isUnique(s) -> bool:
+def is_unique_naive(s):
+    for i in range(len(s)):
+        for j in range(i + 1, len(s)):
+            if s[i] == s[j]:
+                return False
+    return True
+
+def isUnique_ascii(s) -> bool:
+    """
+    Docstring for isUnique_ascii
+    
+    :param s: T.C is O(n), S.C is O(1) => since the loop will never iterate through more then 128 characters
+    :return: Description
+    :rtype: bool
+    """
     if len(s) > 128: # ASCII has only 128 unique characters
         return False
     char_set = [False] * 128
@@ -19,6 +33,61 @@ def isUnique(s) -> bool:
             return False
         char_set[ascii_val] = True
     return True
-print(isUnique("apple"))
-print(isUnique("aple"))
+
+"""
+Better optimization: Bit vector using a single int (lowercase a–z)
+seen = [False, False, True, False, ...];  Boolean list: 128 Python objects
+checker = 00000000000000000000000000000100; Bit mask: One integer
+
+Bit masking requires three strict conditions:
+- The character set size is small (0-25)
+- The size is fixed and known in advance
+- Each character maps cleanly to a bit index
+
+"""
+def is_unique_bitmask(s: str) -> bool:
+    checker = 0 #checker = 000000
+
+    for ch in s:
+        val = ord(ch) - ord('a')  # map a–z → 0–25 ; 'a' → val = 0; 'b' → val = 1
+        if checker & (1 << val): # 1 << 0 = 000001; 1 << 1 = 000010
+            return False
+        checker |= (1 << val) # checker |= mask → 000001; checker |= mask → 000011
+
+    return True
+
+
+
+"""
+- When you extend this to Unicode, the main change is that you can no longer assume a fixed size of 128 characters.
+- Unicode has over 1 million possible code points, so a fixed boolean array is not practical.
+In Unicode:
+    - Max code point ≈ 1,114,112
+    - Boolean array of that size is wasteful and slow to initialize
+
+"""
+# Use a list: still bad O(n^2)
+# seen = []
+# for ch in s:
+#     if ch in seen:  # ❌ O(n)
+#         return False
+#     seen.append(ch)
+
+# A set is built for fast membership checks
+def isUnique_unicode_set(s)->bool:
+    seen = set()
+    for ch in s:
+        if ch in seen:   # O(1)
+            return False
+        seen.add(ch)     # O(1)
+
+# A dict is just a set with values.
+# set     → hash table storing keys only
+# dict    → hash table storing key value pairs
+
+
+
+
+print(isUnique_ascii("apple"))
+print(isUnique_ascii("aple"))
 
